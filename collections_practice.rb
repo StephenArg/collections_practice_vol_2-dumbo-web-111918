3e5bf941b254b092aff657bf71d8e22727fbb829
Stephen Argentina <stephena9871@gmail.com>
2018-11-11 03:40:38 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -79,8 +79,8 @@ end
 def organize_schools(schools)
   new_hash = {}
   schools.each {|place|
-  binding.pry
   if new_hash.has_key?(schools[place][:location])
+    binding.pry
     new_hash[location] << schools[place]
   else
     location = schools[place][:location]
