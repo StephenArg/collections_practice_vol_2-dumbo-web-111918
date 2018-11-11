37d36e4712c69bed38dc37a6c5c2bd21e2748835
Stephen Argentina <stephena9871@gmail.com>
2018-11-11 01:45:19 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,55 @@
-# your code goes here
+def begins_with_r(array)
+  yes = true
+  array.each {|i|
+  if i.start_with?("r")
+    yes = true
+  else
+    yes = false
+    break
+  end}
+  return yes
+end
+
+def contain_a(array)
+  new = []
+  array.each { |i|
+  if i =~ /a/
+    new.push(i)
+  end}
+  return new
+end
+
+def first_wa(array)
+  word = array.find {|i|
+    i[0] == "w" and i[1] == "a"}
+  return word
+end
+
+def remove_non_strings(array)
+  list = []
+  array.each {|i|
+  if i.is_a?(String)
+    list.push(i)
+  end}
+  return list
+end
+
+def count_elements(array) #####
+  list = []
+  holder = {}
+  array.each {|i|
+  if holder.has_key?(i)
+    holder[i] += 1
+  else
+    holder[i] = 1
+  end}
+  holder.each {|name, count|
+    list.push({name: name, count: count})
+  }
+  return list
+end
+
+def merge_data(keys, items)####
+  puts keys, items
+  return merged
+end
